abc03677199c56ca646d60422091ff7e2d79b895
Alw3ys <alw3ys@gmail.com>
2021-12-13 21:28:56 +0100

    homebrew: Install homebrew stuff

--- brew.sh
+++ brew.sh
@@ -6,7 +6,11 @@ brew upgrade
 
 
 # Install here some useful binaries.
+brew install --cask aws-vault
+brew install warrensbox/tap/tfswitch
 
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
 
 # Remove outdated versions from the cellar.
 brew cleanup
