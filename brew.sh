a0b3a074e5b02b252d21d7a8138dae5962a82194
Alvaro Molina <alw3ys@gmail.com>
2023-07-05 19:32:24 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -13,5 +13,7 @@ brew install warrensbox/tap/tfswitch
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
+brew install ngrok/ngrok/ngrok
+
 # Remove outdated versions from the cellar.
 brew cleanup
