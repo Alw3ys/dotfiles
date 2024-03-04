2b6b3fdf5220ca6a0005979a4c11c28b6ec7d0f2
Alw3ys <alw3ys@gmail.com>
2024-03-04 22:57:26 +0100

    packages

--- packages.sh
+++ packages.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 brew install stow
+
+# Python
+brew install python3
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Node
+brew install node
+brew install nvm
+
+brew install watch
+
+brew install pulumi/tap/pulumi
