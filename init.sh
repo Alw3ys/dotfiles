f53e76accf6b1a395d0340fcdc6e0b2e4156c911
Alw3ys <alw3ys@gmail.com>
2024-03-04 22:51:25 +0100

    idk

--- init.sh
+++ init.sh
@@ -8,5 +8,3 @@ echo 'set completion-ignore-case On' >> /etc/inputrc
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Sotw
-brew install stow
