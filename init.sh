e965dada56c9fec4632931583f97a2d888707cfc
Alw3ys <alw3ys@gmail.com>
2024-03-05 09:20:18 +0100

    fix

--- init.sh
+++ init.sh
@@ -3,8 +3,6 @@
 # Set default shell to bash
 chsh -s /bin/bash
 
-echo 'set completion-ignore-case On' >> /etc/inputrc
-
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
