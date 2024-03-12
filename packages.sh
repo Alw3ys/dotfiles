b0b3040c13082368353bc880a9ca710311030fd2
Alw3ys <alw3ys@gmail.com>
2024-03-12 18:47:50 +0100

    idk

--- packages.sh
+++ packages.sh
@@ -9,6 +9,9 @@ brew install protobuf
 brew install python3
 curl -sSL https://install.python-poetry.org | python3 -
 
+# Ruby
+brew install ruby
+
 # Node
 brew install node
 brew install nvm
@@ -27,4 +30,5 @@ brew install jq
 # Remove outdated versions from the cellar.
 brew cleanup
 
+# Ruby Installs
 gem install deb-s3
